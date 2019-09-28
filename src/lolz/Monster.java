19fa4b4cae3d52f012a609218b4b35d39b7a91c4
Tim Holzenkamp <tim@holzenkamp.eu>
2019-09-28 21:14:49 +0200

    added monster class

---
+++ src/lolz/Monster.java
@@ -0,0 +1,40 @@
+package lolz;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+public class Monster {
+    private double x, y;
+    private Image[][] img;
+    private double animation_state;
+    private final String base_monster = "big_demon";
+
+    public Monster(int x, int y){
+        this.x = x;
+        this.y = y;
+        this.img = new Image[2][4];
+        try {
+            for (int i = 0; i < 4; i++) {
+                img[0][i] = ImageIO.read(new File("res/tiles/" + this.base_monster + "_idle_anim_f" + Integer.toString(i) + ".png")).getScaledInstance(50, -1, Image.SCALE_SMOOTH);
+            }
+            for (int i = 0; i < 4; i++) {
+                img[1][i] = ImageIO.read(new File("res/tiles/" + this.base_monster + "_run_anim_f" + Integer.toString(i) + ".png")).getScaledInstance(50, -1, Image.SCALE_SMOOTH);
+            }
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void paint(Graphics g) {
+        this.animation_state += 0.1;
+        this.animation_state %= 4;
+
+        // paint player
+        g.setColor(Color.BLACK);
+        g.drawImage(img[0][(int)this.animation_state], (int) this.x, (int) this.y, null);
+    }
+
+}
