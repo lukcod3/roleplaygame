4550f2bb8cbbda500173da3fe5bbe98a3da34529
Tim-17 <55687420+Tim-17@users.noreply.github.com>
2019-10-03 12:46:56 +0300

    whoops

--- src/lolz/Maps/Map.java
+++ src/lolz/Maps/Map.java
@@ -329,12 +329,10 @@ public abstract class Map {
         }
     }
 
-
     // method returns a new monster with attributes in certain intervals (first intervals are written down at the top) which are determined by the player's level (first intervals are increased by a certain number)
     public Monster generateMonster (int x, int y) {
         return new Monster(x * Main.TILE_SIZE, (int) ((y + 0.5) * Main.TILE_SIZE), /*maxHealth*/randInt((int) (this.minMaxHealth * this.expFactor), (int) (this.maxMaxHealth * this.expFactor)), /*damage*/randInt((int) (this.minDamage * this.expFactor), (int) (this.maxDamage * this.expFactor)), /*armor*/randInt((int) (this.minArmor * this.expFactor), (int) (this.maxArmor * this.expFactor)), /*exp*/randInt((int) (this.minExp * this.expFactor), (int) (this.maxExp * this.expFactor)));
     }
-
 
     // returns integer which is part of the interval [min; max]
     private int randInt (int min, int max) {
